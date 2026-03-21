49dfa092101eefadd5895578c78cefcbc721ba53
UnsafeDodo <unsafedodo@gmail.com>
2026-03-21 12:40:02 +0100

    Update ApiController

--- GestioneTavoliBar/src/GestioneTavoliBar.Api/Controllers/TablesController.cs
+++ GestioneTavoliBar/src/GestioneTavoliBar.Api/Controllers/TablesController.cs
@@ -16,11 +16,45 @@ namespace GestioneTavoliBar.Api.Controllers
             _context = context; 
         }
 
+        [HttpPost]
+        public async Task<ActionResult<Table>> AddTable(Table table)
+        {
+            _context.Tables.Add(table);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTables), new { id = table.Id }, table);
+        }
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Table>>> GetTables()
         {
             var tables = await _context.Tables.ToListAsync();
             return Ok(tables);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Table>> GetTable(int id)
+        {
+            var table = await _context.Tables.FindAsync(id);
+
+            if (table == null)
+                return NotFound();
+
+            return table;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTable(int id)
+        {
+            var table = await _context.Tables.FindAsync(id);
+
+            if (table == null)
+                return NotFound();
+
+            _context.Tables.Remove(table);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
